d29037eb9a0427a457a9a764f0afda69b4c11e9f
AJ ONeal <aj@therootcompany.com>
2025-01-16 16:14:08 -0700

    f: ./build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -u
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
     go build -o ./dash-zmqproxy-linux-x86_64 ./cmd/dash-zmqproxy/
 
-# scp -rp ./dash-zmqproxy-linux-x86_64 tdash-zmq:~/bin/dash-zmqproxy
+scp -rp ./dash-zmqproxy-linux-x86_64 tdash-zmq:~/bin/dash-zmqproxy
